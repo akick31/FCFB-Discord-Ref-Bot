1475b651af29cd2c8f4f5176e389b717162884f9
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-15 22:22:54 -0500

    use older slf4j version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ configurations.all {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("ch.qos.logback:logback-classic:1.4.13")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
     implementation("io.ktor:ktor-server-core:2.3.0")
     implementation("io.ktor:ktor-server-netty:2.3.0")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
