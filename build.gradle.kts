f4bbb7b2921e4c7f569c0037d64d42f0d06bc81f
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-16 00:44:29 -0500

    add log4j12

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.slf4j:slf4j-api:2.0.16")
-    implementation("org.slf4j:slf4j:2.0.16")
+    implementation("org.slf4j:slf4j-log4j12:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.10")
     implementation("io.ktor:ktor-server-core:2.3.12")
     implementation("io.ktor:ktor-server-netty:2.3.12")
