7ad0f4763c2b05bf85d29419791af6d7a4748bef
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-16 00:06:35 -0500

    remove slf4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ configurations.all {
 
 dependencies {
     testImplementation(kotlin("test"))
-    compileOnly("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:1.4.12")
     implementation("io.ktor:ktor-server-core:2.3.0")
     implementation("io.ktor:ktor-server-netty:2.3.0")
