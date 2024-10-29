9d82c9ea90842b5e5c028b2a5f5888291553626b
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-29 01:04:05 -0500

    run ktlint

--- src/main/kotlin/com/fcfb/discord/refbot/api/PlayClient.kt
+++ src/main/kotlin/com/fcfb/discord/refbot/api/PlayClient.kt
@@ -9,7 +9,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.post
 import io.ktor.client.request.put
-import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import java.util.Properties
 
