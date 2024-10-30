a34a697ddad1e7fe808ca4a413f358ebf9702773
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-30 03:45:08 -0500

    fix nullability on start request

--- src/main/kotlin/com/fcfb/discord/refbot/model/request/StartRequest.kt
+++ src/main/kotlin/com/fcfb/discord/refbot/model/request/StartRequest.kt
@@ -6,13 +6,13 @@ import com.fcfb.discord.refbot.model.fcfb.game.Subdivision
 import com.fcfb.discord.refbot.model.fcfb.game.TVChannel
 
 data class StartRequest(
-    val homePlatform: Platform?,
-    val awayPlatform: Platform?,
-    val subdivision: Subdivision?,
-    val homeTeam: String?,
-    val awayTeam: String?,
+    val homePlatform: Platform,
+    val awayPlatform: Platform,
+    val subdivision: Subdivision,
+    val homeTeam: String,
+    val awayTeam: String,
     val tvChannel: TVChannel?,
     val startTime: String?,
     val location: String?,
-    val gameType: GameType?,
+    val gameType: GameType,
 )
