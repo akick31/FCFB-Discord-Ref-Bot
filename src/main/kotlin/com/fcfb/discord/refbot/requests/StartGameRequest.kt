243d793e22df774a3aca142ca3b932e73ef71c17
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-30 04:27:53 -0500

    run ktlint

--- src/main/kotlin/com/fcfb/discord/refbot/requests/StartGameRequest.kt
+++ src/main/kotlin/com/fcfb/discord/refbot/requests/StartGameRequest.kt
@@ -1,13 +1,10 @@
 package com.fcfb.discord.refbot.requests
 
-import com.fcfb.discord.refbot.api.GameClient
-import com.fcfb.discord.refbot.handlers.ErrorHandler
 import com.fcfb.discord.refbot.handlers.discord.DiscordMessageHandler
 import com.fcfb.discord.refbot.handlers.discord.TextChannelThreadHandler
 import com.fcfb.discord.refbot.model.fcfb.game.Game
 import com.fcfb.discord.refbot.model.fcfb.game.Scenario
 import com.fcfb.discord.refbot.utils.Logger
-import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.entity.channel.thread.TextChannelThread
 
