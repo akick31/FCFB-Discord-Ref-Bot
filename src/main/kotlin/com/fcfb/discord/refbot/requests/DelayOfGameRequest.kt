61666d95c285a7d7daf90b611e6452dd802bcbb4
Andrew Kicklighter <apkicklighter@gmail.com>
2024-11-23 03:02:09 -0600

    Implement delay of game logic

---
+++ src/main/kotlin/com/fcfb/discord/refbot/requests/DelayOfGameRequest.kt
@@ -0,0 +1,27 @@
+package com.fcfb.discord.refbot.requests
+
+import com.fcfb.discord.refbot.api.GameClient
+import com.fcfb.discord.refbot.handlers.ErrorHandler
+import com.fcfb.discord.refbot.handlers.discord.DiscordMessageHandler
+import com.fcfb.discord.refbot.model.fcfb.game.Game
+import com.fcfb.discord.refbot.model.fcfb.game.Scenario
+import com.fcfb.discord.refbot.model.fcfb.game.TeamSide
+import dev.kord.core.Kord
+
+class DelayOfGameRequest {
+    suspend fun notifyDelayOfGame(
+        client: Kord,
+        game: Game
+    ) {
+        val message = DiscordMessageHandler().sendDelayOfGameMessage(client, game) ?: return
+
+        val numberRequestMessage = DiscordMessageHandler().sendRequestForDefensiveNumber(
+            client,
+            game,
+            Scenario.DELAY_OF_GAME,
+            null
+        ) ?: return ErrorHandler().failedToSendNumberRequestMessage(message)
+
+        GameClient().updateRequestMessageId(game.gameId, numberRequestMessage)
+    }
+}
