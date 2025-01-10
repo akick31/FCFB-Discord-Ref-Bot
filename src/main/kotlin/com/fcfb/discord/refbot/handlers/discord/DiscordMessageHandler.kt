71425375bc22eb584a6a1cffc0b63f1b4055e914
Andrew Kicklighter <apkicklighter@gmail.com>
2025-01-09 21:46:42 -0600

    fix redzone and update discord message

--- src/main/kotlin/com/fcfb/discord/refbot/handlers/discord/DiscordMessageHandler.kt
+++ src/main/kotlin/com/fcfb/discord/refbot/handlers/discord/DiscordMessageHandler.kt
@@ -516,7 +516,8 @@ class DiscordMessageHandler(
             }
         }
 
-        messageContent += "\n\n[Play List](https://fakecollegefootball.com/game-details/${game.gameId})"
+        messageContent += "\n\n[Play List](http://51.81.32.234:462/game-details/${game.gameId})" +
+                "\n[Ranges](https://docs.google.com/spreadsheets/d/1yXG2Xe1W_G5uq_1Tus3AbP4u8HOwjgmJ1LOQDV-dhvc/edit#gid=1822037032)"
 
         val originalScorebug = scorebugClient.getScorebugByGameId(game.gameId)
 
