a89485d7d099c32a9217477af166f275a9f3c05f
Andrew Kicklighter <apkicklighter@gmail.com>
2025-02-17 20:02:57 -0600

    say team is empty in get team coaches

--- src/main/kotlin/com/fcfb/discord/refbot/commands/GetTeamCoachesCommand.kt
+++ src/main/kotlin/com/fcfb/discord/refbot/commands/GetTeamCoachesCommand.kt
@@ -49,45 +49,8 @@ class GetTeamCoachesCommand(
                 "${interaction.user.username} successfully grabbed team coaches in channel ${interaction.channelId.value}",
             )
         } else {
-            response.respond { this.content = "Could not find team!" }
+            response.respond { this.content = "Team is empty!" }
             Logger.error("${interaction.user.username} failed to get the team coaches in channel ${interaction.channelId.value}")
         }
-    }
-
-    private fun getGameInformation(game: Game): String {
-        val waitingOn = if (game.waitingOn == TeamSide.HOME) game.homeTeam else game.awayTeam
-        val possession = if (game.possession == TeamSide.HOME) game.homeTeam else game.awayTeam
-        val coinTossWinner = if (game.coinTossWinner == TeamSide.HOME) game.homeTeam else game.awayTeam
-        var messageContent = "**${game.homeTeam} vs ${game.awayTeam}**\n"
-        messageContent += "**Game Type**: ${game.gameType?.description}\n"
-        messageContent += "**Game Status**: ${game.gameStatus?.description}\n"
-        if (game.season != null) {
-            messageContent += "**Season**: ${game.season}\n"
-        }
-        if (game.week != null) {
-            messageContent += "**Week**: ${game.week}\n"
-        }
-        messageContent += "**Subdivision**: ${game.subdivision}\n"
-        if (game.tvChannel != null) {
-            messageContent += "**TV Channel**: ${game.tvChannel}\n"
-        }
-        if (game.startTime != null) {
-            messageContent += "**Start Time**: ${game.startTime}\n"
-        }
-        if (game.location != null) {
-            messageContent += "**Location**: ${game.location}\n"
-        }
-        messageContent += "**Possession**: ${possession}\n"
-        messageContent += "**Waiting On**: ${waitingOn}\n"
-        messageContent += "**Coin Toss Winner**: ${coinTossWinner}\n"
-        messageContent += "**Coin Toss Choice**: ${game.coinTossChoice?.description}\n"
-        messageContent += "**Game Timer**: ${game.gameTimer}\n"
-        messageContent += "**Game ID**: ${game.gameId}\n"
-        messageContent += "**Request Message ID**: ${game.requestMessageId}\n"
-        messageContent += "**${game.homeTeam} Offensive Playbook**: ${game.homeOffensivePlaybook?.description}\n"
-        messageContent += "**${game.homeTeam} Defensive Playbook**: ${game.homeDefensivePlaybook?.description}\n"
-        messageContent += "**${game.awayTeam} Offensive Playbook**: ${game.awayOffensivePlaybook?.description}\n"
-        messageContent += "**${game.awayTeam} Defensive Playbook**: ${game.awayDefensivePlaybook?.description}\n"
-        return messageContent
     }
 }
